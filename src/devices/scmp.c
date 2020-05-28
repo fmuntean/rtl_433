480ad4da1565eb88584aafb8b21bf420ff23c9e9
Florin Muntean <fmuntean@gmail.com>
2020-05-28 17:29:30 -0400

    build fix and calculate crc for SCM+

--- src/devices/scmp.c
+++ src/devices/scmp.c
@@ -39,11 +39,11 @@ Packet Checksum	2		        CRC-16-CCITT of packet starting at Protocol ID.
 
 static int scmp_decode(r_device *decoder, bitbuffer_t *bitbuffer)
 {
-    static const uint8_t IDM_PREAMBLE[]  = {0x16, 0xA3, 0x1E};
+    static const uint8_t IDM_PREAMBLE[]  = {0x16, 0xA3 }; //, 0x1E};
     uint8_t *b;
-    uint8_t scm_type;
+    uint8_t protocol,scm_type;
     uint32_t consumption, ert_id;
-    uint16_t crc;
+    uint16_t tamper,crc;
     data_t *data;
 
     if (bitbuffer->bits_per_row[0] != 16*8) //16 bytes long package
@@ -79,24 +79,64 @@ static int scmp_decode(r_device *decoder, bitbuffer_t *bitbuffer)
          return DECODE_ABORT_EARLY; // no preamble matching
 
 
+     crc = (b[14]<<8) | b[15];
+
+//uint16_t calc_crc = crc16(&b[2], 12, 0x1021, 0);
+
+    //if (crc_received != crc_calculated) {
+    //    if (decoder->verbose > 1) {
+  //  fprintf(stderr, "CRC check (0x%X != 0x%X)\n", calc_crc, crc);
+    //    }
+    //    return DECODE_FAIL_MIC;
+   // }
+
+
+	uint16_t calc_crc = crc16(&b[2], 12, 0x1021, 0xFFFF) ^ 0xFFFF;
+ //fprintf(stderr, "CRC check (0x%X != 0x%X)\n", calc_crc, crc);
+
+   // calc_crc = crc16(&b[2], 12, 0x8408, 0);
+ //fprintf(stderr, "CRC check (0x%X != 0x%X)\n", calc_crc, crc);
+
+//calc_crc = crc16(&b[2], 12, 0x8408, 0xFFFF);
+
+ //fprintf(stderr, "CRC check (0x%X != 0x%X)\n", calc_crc, crc);
+
+
+
+// fprintf(stderr, "CRC check (0x%X != 0x%X)\n", calc_crc, crc);
+
+//      uint16_t calc_crc = crc16lsb(&b[2],12,0x8408,0);
     /* Extract parameters */
     protocol = b[2];
     scm_type = b[3];
-    ert_id = (b[4]<<24) | (b54]<<16) | (b[6]<<8) | b[7];
+    ert_id = (b[4]<<24) | (b[5]<<16) | (b[6]<<8) | b[7];
     consumption = (b[8]<<24) | (b[9]<<16) | (b[10]<<8) | b[11];
     tamper = (b[12]<<8) | b[13];
-    crc = (b[14]<<8) | b[15]; 
+    //crc = (b[14]<<8) | b[15]; 
+
+
+    char strData[16*2+1];
+    const char *hex="0123456789ABCDEF";
+    for(int i=0;i<16;i++)
+    {
+      strData[i*2] = hex[(b[i]>>4) & 0xF];
+      strData[i*2+1] = hex[b[i] & 0x0F];
+    }
+    strData[16*2]=0;
+
     
     /* clang-format off */
     data = data_make(
             "model",           "",                  DATA_STRING, "SCM+",
             "protocol", "Protocol ID",              DATA_INT, protocol,
             "scm_type",        "SCM+ Type",         DATA_INT, scm_type,
-            "id",              "Id",                DATA_INT,    ert_id,
+            "id",              "Id",                DATA_INT, ert_id,
             "consumption",  "Consumption",          DATA_INT, consumption,
             "tamper", "Tamper",                     DATA_INT, tamper,
             "crc", "Packet CRC",                    DATA_INT, crc,
-            "mic",             "Integrity",         DATA_STRING, "CRC",
+	    "calc_crc", "CRC",                      DATA_INT, calc_crc,
+            "raw", "RAW DATA",			   DATA_STRING, strData,
+	    "mic",             "Integrity",         DATA_STRING, "CRC",
             NULL);
     /* clang-format on */
 
@@ -112,6 +152,8 @@ static char *output_fields[] = {
         "consumption",
         "tamper",
         "crc",
+        "calc_crc",
+	"raw",
         "mic",
         NULL
 };
